c0e5bb0c45ff48f8b21598b404f407eef32f084f
Guy Rozendorn <guy@rzn.co.il>
2012-09-08 17:12:12 +0300

    TRIVIAL fixing build

--- src/infi/recipe/application_packager/utils/buildout/__init__.py
+++ src/infi/recipe/application_packager/utils/buildout/__init__.py
@@ -38,8 +38,7 @@ def write_buildout_configuration_file_for_production(dependent_scripts, eggs, sc
     with open("buildout.in", 'w') as fd:
         fd.write(dedent(BUILDOUT_IN))
     with open_buildout_configfile("buildout.in", True) as buildout:
-        buildout.set("production-scripts", "dependent-scripts",
-                     "true" if dependent-scripts in ['true', True] else 'false')
+        buildout.set("production-scripts", "dependent-scripts")
         buildout.set("production-scripts", "eggs", eggs)
         if scripts:
             buildout.set("production-scripts", "eggs", scripts)
