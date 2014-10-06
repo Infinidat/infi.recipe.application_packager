e7308c1c88af3e1f7347ceef66e6989a157ff11b
Itai Shirav <itais@infinidat.com>
2014-10-06 11:26:49 +0300

    fix test

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -35,7 +35,7 @@ class CompilerTestCase(unittest.TestCase):
                     get_archive_path("pycrypto-2.6.1.tar.gz"),
                     get_archive_path("async-0.6.1.tar.gz"),
                     get_archive_path("MarkupSafe-0.23.tar.gz"),
-                    get_archive_path("psutil-2.1.1.tar.gz")]
+                    get_archive_path("psutil-2.1.3.tar.gz")]
         actual = self.get_compiler().get_packages_to_install()
         self.assertEquals(set(actual), set(expected))
 
