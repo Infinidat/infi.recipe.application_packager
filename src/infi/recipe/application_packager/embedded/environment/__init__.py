fede69c607b38908dee9dfc7b8ab05886aef2303
Guy Rozendorn <guy@rzn.co.il>
2014-10-14 19:17:34 +0300

    TRIVIAL fixing build

--- src/infi/recipe/application_packager/embedded/environment/__init__.py
+++ src/infi/recipe/application_packager/embedded/environment/__init__.py
@@ -108,7 +108,11 @@ def write_pystick_variable_file(pystick_variable_filepath, python_files, c_exten
 def locate_vcvars():
     from infi.registry import LocalComputer
     VISUAL_STUDIO_9_REGKEY = "SOFTWARE\\{}Microsoft\\VisualStudio\\9.0".format(r"Wow6432Node\\" if is_64bit() else "")
-    visual_studio_9 = LocalComputer().local_machine[VISUAL_STUDIO_9_REGKEY]
+    try:
+        visual_studio_9 = LocalComputer().local_machine[VISUAL_STUDIO_9_REGKEY]
+    except KeyError:
+        return r"C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat" if is_64bit() else \
+               r"C:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat
     # installdir = C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\
     # vcvars = C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat
     installdir = visual_studio_9.values_store['InstallDir'].to_python_object()
