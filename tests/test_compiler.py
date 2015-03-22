234a93382a443d7f3a2f8b81f7774f149ebc1a36
Guy Rozendorn <guy@rzn.co.il>
2015-03-22 17:39:32 +0200

    TRIVIAL bumping psutil again

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -31,7 +31,7 @@ class CompilerTestCase(unittest.TestCase):
         expected = [get_archive_path("coverage-3.7.1.tar.gz"),
                     get_archive_path("gitdb-0.6.4.tar.gz"),
                     get_archive_path("MarkupSafe-0.23.tar.gz"),
-                    get_archive_path("psutil-3.0.1.tar.gz")]
+                    get_archive_path("psutil-3.0.2.tar.gz")]
         actual = self.get_compiler().get_packages_to_install()
         self.assertEquals(set(actual), set(expected))
 
