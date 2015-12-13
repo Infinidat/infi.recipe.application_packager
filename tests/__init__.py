ae301c7f603c09e35306e5db7187d3746aebbc49
Arnon Yaari <arnony@infinidat.com>
2015-12-13 11:01:54 +0200

    fix tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,13 @@
 import unittest
 from infi.pyutils.decorators import wraps
-from os import environ, path, curdir
+from os import environ, path, curdir, listdir
 
-def get_archive_path(filename):
-    return path.join(path.abspath(curdir), '.cache', 'dist', filename)
+def get_archive_path(package_name):
+    dist_dir = path.join(path.abspath(curdir), '.cache', 'dist')
+    matches = [filename for filename in listdir(dist_dir) if filename.startswith(package_name)]
+    if not matches:
+        raise RuntimeError("Package not found in dist dir: {}".format(package_name))
+    return path.join(dist_dir, matches[0])
 
 def long_one(func):
     @wraps(func)
