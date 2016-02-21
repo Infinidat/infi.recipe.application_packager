498515388e46b20e30b7e980de1c4d08c35ce389
Guy Rozendorn <guy@rzn.co.il>
2016-02-21 21:56:00 +0200

    SPTL-503 removing rcssmin from the buildout and tests

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -33,7 +33,6 @@ class CompilerTestCase(unittest.TestCase):
                     get_archive_path("MarkupSafe"),
                     get_archive_path("psutil"),
                     get_archive_path("Logbook"),
-                    get_archive_path("rcssmin"),
                     ]
         actual = self.get_compiler().get_packages_to_install()
         self.assertEquals(set(actual), set(expected))
