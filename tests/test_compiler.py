55095ef37b5df4398a0515eaa394d9cc6313e417
Oded Badt <obadt@infinidat.com>
2016-11-16 16:10:25 +0200

    Fixed ubuntu tests

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -39,8 +39,7 @@ class CompilerTestCase(unittest.TestCase):
 
     def test_get_packages_to_install(self):
         try:
-            expected = [get_archive_path('infi.recipe.python'),
-                        get_archive_path("coverage"),
+            expected = [get_archive_path("coverage"),
                         get_archive_path("MarkupSafe"),
                         get_archive_path("psutil"),
                         get_archive_path("Logbook"),
