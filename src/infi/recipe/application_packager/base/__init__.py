3888a8e87f373df6df05da3c11890562aea855d6
Guy Rozendorn <guy@rzn.co.il>
2017-06-14 12:00:00 +0300

    Adding six

--- src/infi/recipe/application_packager/base/__init__.py
+++ src/infi/recipe/application_packager/base/__init__.py
@@ -26,18 +26,21 @@ RECIPE_DEFAULTS = {'require-administrative-privileges': 'false',
                    'eula-rtf': None,
                    'documentation-url': None,
                    '_target_arch': None,
-                   'close-on-upgrade-or-removal' : 'true',
+                   'close-on-upgrade-or-removal': 'true',
                    'additional-directories': '[]'
                   }
 
-PYTHON_PACKAGES_USED_BY_PACKAGING = ["infi.recipe.buildout_logging",
-                                     "infi.recipe.console_scripts",
-                                     "infi.recipe.close_application",
-                                     "zc.buildout",
-                                     "pip",
-                                     "setuptools",
-                                     "pythonpy",
-                                     "buildout.wheel"]
+PYTHON_PACKAGES_USED_BY_PACKAGING = [
+    "buildout.wheel",
+    "infi.recipe.buildout_logging",
+    "infi.recipe.close_application",
+    "infi.recipe.console_scripts",
+    "pip",
+    "pythonpy",
+    "setuptools",
+    "six",
+    "zc.buildout",
+]
 
 SCRIPTS_BY_PACKAGING = ["buildout"]
 
