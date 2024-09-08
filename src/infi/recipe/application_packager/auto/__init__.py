72937a594d1449aefe5424a4c980c720422d38bc
Alexander Deiter <adeiter@infinidat.com>
2024-09-08 08:25:08 +0000

    HOSTDEV-3558: Fix application_packager to support all RHEL clones

--- src/infi/recipe/application_packager/auto/__init__.py
+++ src/infi/recipe/application_packager/auto/__init__.py
@@ -1,25 +1,28 @@
 import platform
-from distro import id as distro_id
+from infi.os_info import get_platform_string, system_is_rhel_based
 
 def get_platform_specific_recipe():
-    if platform.system() == 'Windows':
+    system = platform.system()
+    if system == 'Windows':
         from .. import msi
         return msi.Recipe
-    elif platform.system() == 'Linux':
-        distro = distro_id().replace('rhel', 'redhat').replace('sles', 'suse').replace('oracle', 'redhat').replace('enterpriseenterpriseserver', 'redhat').replace('rocky', 'redhat')
-        if distro.startswith('red') or distro.startswith('cent') or distro.startswith('suse'):
+    elif system == 'Linux':
+        dist = get_platform_string().split('-')[1]
+        if system_is_rhel_based() or dist == 'suse':
             from .. import rpm
             return rpm.Recipe
-        elif distro.startswith('ubuntu'):
+        elif dist in ('debian', 'ubuntu'):
             from .. import deb
             return deb.Recipe
-    elif platform.system() == 'SunOS':
+        else:
+            NotImplementedError("no packaging recipe available for %s Linux distribution" % dist)
+    elif system == 'SunOS':
         from .. import pkg
         return pkg.Recipe
-    elif platform.system() == "AIX":
+    elif system == "AIX":
         from .. import rpm
         return rpm.Recipe
     else:
-        raise NotImplementedError("no packaging recipe available for this platform")
+        raise NotImplementedError("no packaging recipe available for %s platform" % system)
 
 Recipe = get_platform_specific_recipe()
