89ef0630cbba7071c8a3a2c9f9a40aad271e637d
Alexander Deiter <adeiter@infinidat.com>
2024-11-29 17:39:07 +0000

    HOSTDEV-3578: Fix invalid PACKAGE_VRMF for AIX

--- src/infi/recipe/application_packager/aix/__init__.py
+++ src/infi/recipe/application_packager/aix/__init__.py
@@ -2,11 +2,17 @@ __import__('pkg_resources').declare_namespace(__name__)
 
 from ..base import PackagingRecipe
 from .. import bff, rpm
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Recipe(PackagingRecipe):
     def install(self):
         packages = []
         for manager in (bff, rpm):
             recipe = manager.Recipe(self.buildout, self.__class__.__name__, self.options)
-            packages += recipe.install()
+            try:
+                packages += recipe.install()
+            except NotImplementedError as error:
+                logger.warning('Package will not be created: %s', error)
         return packages
